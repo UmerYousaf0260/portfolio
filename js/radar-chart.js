416b2689b0ce244e1107f513284c6d7feed6338d
Umer Yousaf <80925174+UmerYousaf0260@users.noreply.github.com>
2024-02-15 16:44:24 +0500

    new theme

---
+++ js/radar-chart.js
@@ -0,0 +1,57 @@
+const ctx = document.getElementById("radarChart").getContext("2d");
+
+const skillsData = {
+  labels: [
+    "HTML5",
+    "CSS3",
+    "JavaScript",
+    "React JS",
+    "Tailwind CSS",
+    "SASS",
+    "Material UI",
+    "Redux",
+    "Git & Github",
+  ],
+  datasets: [
+    {
+      backgroundColor: "rgba(199, 0, 57, 0.2)",
+      data: [90, 85, 80, 75, 85, 80, 85, 75, 70],
+      borderColor: "rgba(199, 0, 57, 1)",
+      pointBackgroundColor: "rgba(199, 0, 57, 1)",
+      pointRadius: 5,
+      pointHoverRadius: 8,
+    },
+  ],
+};
+
+const radarChart = new Chart(ctx, {
+  type: "radar",
+  data: skillsData,
+  options: {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          display: false,
+        },
+      },
+      r: {
+        min: 0,
+        max: 100,
+        grid: {
+          color: "rgba(255, 255, 255, 0.2)",
+        },
+        pointLabels: {
+          color: "rgba(255, 255, 255, 0.2)",
+        },
+        ticks: {
+          display: false,
+        },
+      },
+    },
+  },
+});
